b13ac9d5fbd357fb514281f5d4c4f1e6da9acc37
Luizhnrs <Luizhnrsg@gmail.com>
2024-06-20 14:32:50 -0300

    Feat: Post -> Create Wallet is implemented

---
+++ picpay/src/main/java/com/luiz/picpay/controller/WalletController.java
@@ -0,0 +1,26 @@
+package com.luiz.picpay.controller;
+
+import com.luiz.picpay.controller.dto.CreateWalletDto;
+import com.luiz.picpay.entity.Wallet;
+import com.luiz.picpay.repository.WalletTypeRepository;
+import com.luiz.picpay.service.WalletService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WalletController {
+    private final WalletService walletService;
+    public WalletController(WalletService walletService) {
+        this.walletService = walletService;
+    }
+    @PostMapping("/wallets")
+    public ResponseEntity<Wallet> createWallet(@RequestBody CreateWalletDto dto){
+       var wallet = walletService.createWallet(dto);
+       return ResponseEntity.ok(wallet);
+    }
+
+}
