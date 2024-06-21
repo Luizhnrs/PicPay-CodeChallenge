944b66b9fb438319dc13b93b6b26d3af6982d204
Luizhnrs <Luizhnrsg@gmail.com>
2024-06-21 14:04:41 -0300

    Feat: Exception Implemented in Service

--- picpay/src/main/java/com/luiz/picpay/service/WalletService.java
+++ picpay/src/main/java/com/luiz/picpay/service/WalletService.java
@@ -2,6 +2,7 @@ package com.luiz.picpay.service;
 
 import com.luiz.picpay.controller.dto.CreateWalletDto;
 import com.luiz.picpay.entity.Wallet;
+import com.luiz.picpay.exceptions.WalletDataExistingException;
 import com.luiz.picpay.repository.WalletRepository;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,11 @@ public class WalletService {
     }
 
     public Wallet createWallet(CreateWalletDto dto) {
+      var walletDb = walletRepository.findByCpfCnpjOrEmail(dto.cpfCnpj(), dto.email());
+      if(walletDb.isPresent())
+      {
+          throw new WalletDataExistingException ("Cpf, Cnpj or Email Already Exists");
+      }
        return walletRepository.save(dto.toWallet());
     }
 }
