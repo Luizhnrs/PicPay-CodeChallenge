ae4864efebe27648bca897d2958d50dd6f548991
Luizhnrs <Luizhnrsg@gmail.com>
2024-06-21 12:47:39 -0300

    Feat: ExistingWalletException Implemented

---
+++ picpay/src/main/java/com/luiz/picpay/exceptions/ExistingWalletException.java
@@ -0,0 +1,12 @@
+package com.luiz.picpay.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+public class ExistingWalletException extends RuntimeException {
+    public ProblemDetail toProblemDetail(){
+        var pb = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        pb.setTitle("Internal Server Error");
+        return pb;
+    }
+}
