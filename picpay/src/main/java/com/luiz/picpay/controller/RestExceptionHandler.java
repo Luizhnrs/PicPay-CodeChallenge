7e5d3056f18885ea5ab449798ef4e913a9b08f86
Luizhnrs <Luizhnrsg@gmail.com>
2024-06-22 12:17:07 -0300

    Feat: Problem Detail List implemeted

--- picpay/src/main/java/com/luiz/picpay/controller/RestExceptionHandler.java
+++ picpay/src/main/java/com/luiz/picpay/controller/RestExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.luiz.picpay.controller;
 
 import com.luiz.picpay.exceptions.ExistingWalletException;
+import org.springframework.beans.factory.parsing.Problem;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.security.InvalidAlgorithmParameterException;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
@@ -13,4 +18,16 @@ public class RestExceptionHandler {
         return ex.toProblemDetail();
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        var fieldErrors = ex.getFieldErrors()
+                .stream()
+                .map(f -> new InvalidParam(f.getField(), f.getDefaultMessage()))
+                .toList();
+        var pb = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        pb.setTitle("Request Didn`t validate");
+        pb.setProperty("Invalid Params", fieldErrors);
+        return null;
+    }
+    private record InvalidParam(String fieldName, String reason){}
 }
