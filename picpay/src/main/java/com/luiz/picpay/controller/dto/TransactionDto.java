99aed278e3c2d0ffa29995e57bc3f555470bf735
Luizhnrs <Luizhnrsg@gmail.com>
2024-06-24 22:42:22 -0300

    Feat: Transaction Dto is implemented

---
+++ picpay/src/main/java/com/luiz/picpay/controller/dto/TransactionDto.java
@@ -0,0 +1,12 @@
+package com.luiz.picpay.controller.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record TransactionDto(
+      @DecimalMin("0.01") @NotNull BigDecimal value,
+       @NotNull Long payer,
+       @NotNull Long payee) {
+}
