d944338d7e48a38608ff5c177ed7af5c08b6a7de
Luizhnrs <Luizhnrsg@gmail.com>
2024-06-24 22:44:07 -0300

    Feat: Equals hash code implemented in wallet type

--- picpay/src/main/java/com/luiz/picpay/entity/WalletType.java
+++ picpay/src/main/java/com/luiz/picpay/entity/WalletType.java
@@ -2,6 +2,8 @@ package com.luiz.picpay.entity;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "tb_wallet_type" )
 public class WalletType {
@@ -47,5 +49,17 @@ public class WalletType {
         public WalletType get(){
             return new WalletType(id,description);
         }
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WalletType that = (WalletType) o;
+        return Objects.equals(id, that.id) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description);
     }
 }
