a19cb0d198a94289f7782d11a3c53a54ca6f35c7
Luizhnrs <Luizhnrsg@gmail.com>
2024-06-24 22:42:31 -0300

    Feat: Transaction Repository is implemented

---
+++ picpay/src/main/java/com/luiz/picpay/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.luiz.picpay.repository;
+
+import com.luiz.picpay.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+}
