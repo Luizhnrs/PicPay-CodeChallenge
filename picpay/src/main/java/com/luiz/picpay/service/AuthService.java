03e46f18331d9394a2fd43d0533edf60e8a76da5
Luizhnrs <Luizhnrsg@gmail.com>
2024-06-25 13:46:44 -0300

    Refactor: Transaction Dto (is Auth)

--- picpay/src/main/java/com/luiz/picpay/service/AuthService.java
+++ picpay/src/main/java/com/luiz/picpay/service/AuthService.java
@@ -1,7 +1,7 @@
 package com.luiz.picpay.service;
 
 import com.luiz.picpay.client.AuthClient;
-import com.luiz.picpay.entity.Transaction;
+import com.luiz.picpay.controller.dto.TransactionDto;
 import com.luiz.picpay.exceptions.BankException;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +12,7 @@ public class AuthService {
     public AuthService(AuthClient authClient) {
         this.authClient = authClient;
     }
-    public boolean isAuth(Transaction transaction)
+    public boolean isAuth(TransactionDto transaction)
     {
         var resp = authClient.isAuth();
         if (resp.getStatusCode().isError()){
