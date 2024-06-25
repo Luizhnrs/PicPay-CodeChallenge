dfb47082cb64eb9aeea6da5ed86d32cc5abe5b08
Luizhnrs <Luizhnrsg@gmail.com>
2024-06-24 22:39:58 -0300

    Feat:Authentication Service implemented

---
+++ picpay/src/main/java/com/luiz/picpay/service/AuthService.java
@@ -0,0 +1,24 @@
+package com.luiz.picpay.service;
+
+import com.luiz.picpay.client.AuthClient;
+import com.luiz.picpay.entity.Transaction;
+import com.luiz.picpay.exceptions.BankException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final AuthClient authClient;
+
+    public AuthService(AuthClient authClient) {
+        this.authClient = authClient;
+    }
+    public boolean isAuth(Transaction transaction)
+    {
+        var resp = authClient.isAuth();
+        if (resp.getStatusCode().isError()){
+            throw new BankException();
+        }
+        return resp.getBody().authorized();
+    }
+
+}
